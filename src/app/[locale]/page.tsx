800a06b3af7437001a9ab300a292ce10336805ee
Garcia-Sebastiao <garciasebastiao.php@gmail.com>
2024-06-12 12:58:19 +0100

    loader

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { HomeView } from "@/views/home/HomeView";
 import { BannerIllustration } from "@/assets/illustrations/BannerIllustration";
+import { FullLogo } from "@/assets/FullLogo";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     AOS.init({
       duration: 800,
@@ -13,12 +16,24 @@ export default function App() {
     });
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 6000);
+  });
+
   return (
     <>
-      <div className="hidden lg:flex absolute -z-10 top-0 left-0">
-        {BannerIllustration}
-      </div>
-      <HomeView />
+      {isLoading ? (
+        <div className="w-full transition-all loader fixed top-0 left-0 z-50 bg-white h-screen flex justify-center items-center">
+          {FullLogo}
+        </div>
+      ) : (
+        <>
+          <div className="hidden lg:flex absolute -z-10 top-0 left-0">
+            {BannerIllustration}
+          </div>
+          <HomeView />
+        </>
+      )}
     </>
   );
 }
